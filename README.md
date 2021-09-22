0e2ec259a63828d695267249d52c660ed76ad01b
sydneyyk <45011866+sydneyyk@users.noreply.github.com>
2021-09-21 21:45:24 -0400

    Update NYTimesApiClient.java with personal API key

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Lab 1 - UI and Git Lab
 
 This is the starter code for lab 1, see [Android University Course - Lab 1](https://courses.codepath.org/courses/android_university/unit/1#!exercises) for additional instructions.
+##NY Times API Key: MX4GjlKc53pww1ZDBhAZkdr8gGdNHkTs remove from Readme when finished
--- app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.java
+++ app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.java
@@ -25,7 +25,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class NYTimesApiClient {
 
     // TODO: Replace the below API key with your own generated key
-    private static final String API_KEY = "<YOUR-API-KEY-GOES-HERE>";
+    private static final String API_KEY = "MX4GjlKc53pww1ZDBhAZkdr8gGdNHkTs";
     private NYTimesService nyTimesService;
 
     public NYTimesApiClient() {
